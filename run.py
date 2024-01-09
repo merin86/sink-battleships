929bb42e7a076cdc5d00269156d29412ea28395a
merin86 <m_liljenroth@hotmail.com>
2024-01-09 13:58:48 +0000

    Import random package for python

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
 print("Welcome to Sink Battleships!")
+
+# Write your code to expect a terminal of 80 characters wide and 24 rows high
