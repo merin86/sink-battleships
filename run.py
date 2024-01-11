a6c3a3066700b853b3c4a6c4811fc5726efa8523
merin86 <m_liljenroth@hotmail.com>
2024-01-11 10:33:53 +0000

    Create functions for the game

--- run.py
+++ run.py
@@ -8,3 +8,28 @@ hidden_board_computer = [[" "] * 5 for x in range(5)]
 # This board is updated during the game by showing the misses and hits of the ships
 visible_board_user = [[" "] * 5 for x in range(5)]
 visible_board_computer = [[" "] * 5 for x in range(5)]
+
+def print_board(board):
+    """
+    This function prints the board with row numbers on the left
+    and column labels on the top, creating a visual representation
+    of the game board.
+    """
+    pass
+
+
+def create_ships():
+    pass
+
+
+def get_ship_location():
+    pass
+
+
+def computer_guess():
+    pass
+
+
+def count_hit_ships():
+    pass
+
