df1e70266c1eb9773ae8e007cab34d91b3484108
merin86 <m_liljenroth@hotmail.com>
2024-01-11 10:03:41 +0000

    Create boards for the game

--- run.py
+++ run.py
@@ -1,5 +1,10 @@
 import random
 
-print("Welcome to Sink Battleships!")
 
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# On this board the ships are placed out and hidden
+hidden_board_user = [[" "] * 5 for x in range(5)]
+hidden_board_computer = [[" "] * 5 for x in range(5)]
+
+# This board is updated during the game by showing the misses and hits of the ships
+visible_board_user = [[" "] * 5 for x in range(5)]
+visible_board_computer = [[" "] * 5 for x in range(5)]
